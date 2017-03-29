4fb8e0570e869bd49cb365f23f3e38d0b434d1c9
Jeremiah <jeremiaht.walters@gmail.com>
2017-03-28 23:39:27 -0700

    karma installed with other dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+'use strict';
+
+require('dotenv').load();
+
+const webpack = require('webpack');
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval',
+  entry: `${__dirname}/app/entry.js`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/build`
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app/index.html`
+    }),
+    new ExtractTextPlugin('bundle.css'),
+    // webpack constant env varible used for our app
+    new webpack.DefinePlugin({
+      __API_URL__: JSON.stringify(process.env.API_URL)
+    })
+    // API_URL is for our backend
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  }
+};
